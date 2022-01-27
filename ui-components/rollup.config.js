31210c83bb2a60cd1a46594ec6492dbdcf911185
Leonardo Gedler <lgedler@paypal.com>
2022-01-27 19:41:07 -0400

    test with rollup

---
+++ ui-components/rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import alias from "@rollup/plugin-alias";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+      alias({
+        entries: [
+          { find: "react", replacement: "preact/compat" },
+          { find: "react-dom/test-utils", replacement: "preact/test-utils" },
+          { find: "react-dom", replacement: "preact/compat" },
+          { find: "react/jsx-runtime", replacement: "preact/jsx-runtime" },
+        ],
+      }),
+    ],
+    external: ["react", "react-dom", "styled-components"],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
