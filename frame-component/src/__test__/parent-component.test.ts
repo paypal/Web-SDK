7a4d8e8a7fb9a5ae55e14f3d5d720e27d2d971fd
Leonardo Gedler <lgedler@paypal.com>
2022-02-03 12:27:51 -0400

    refactor: make ParentComponent an abstract class

--- frame-component/src/__test__/parent-component.test.ts
+++ frame-component/src/__test__/parent-component.test.ts
@@ -1,18 +1,23 @@
 import { ParentComponent } from "../parent-component";
 
+class CustomParentComponent extends ParentComponent {
+  static url: string = "https://example.com/child-frame";
+}
+
 describe("ParentComponent", () => {
   describe("render", () => {
     it("inserts the iframe element into the provided DOM node", async () => {
-      const parent = new ParentComponent();
+      const parent = new CustomParentComponent();
       const container = document.createElement("div");
 
       await parent.render(container);
 
-      expect(container.querySelector("iframe")).toBeTruthy();
+      const iframe = container.querySelector("iframe");
+      expect(iframe?.src).toBe("https://example.com/child-frame");
     });
 
     it("resolves with the component instance", async () => {
-      const parent = new ParentComponent();
+      const parent = new CustomParentComponent();
       const container = document.createElement("div");
 
       const instance = await parent.render(container);
--- frame-component/src/parent-component.ts
+++ frame-component/src/parent-component.ts
@@ -1,11 +1,18 @@
-export class ParentComponent {
-  private iframe: HTMLIFrameElement;
+export type ParentProps = {};
 
-  constructor() {
+export abstract class ParentComponent {
+  static url: string;
+  private iframe: HTMLIFrameElement;
+  private staticProperties: typeof ParentComponent;
+
+  constructor(props: ParentProps = {}) {
     this.iframe = document.createElement("iframe");
+    this.staticProperties = this.constructor as typeof ParentComponent;
   }
 
   async render(container: HTMLElement): Promise<this> {
+    this.iframe.src = this.staticProperties.url;
+
     container.appendChild(this.iframe);
     return Promise.resolve(this);
   }
