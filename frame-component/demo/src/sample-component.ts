6f422f2d97162fe8f5ee86c66644eeab6e16fd8b
Blade Barringer <blade.barringer@paypal.com>
2022-02-14 16:26:41 -0600

    chore: remove unused file in demo app

--- frame-component/demo/src/sample-component.ts
+++
@@ -1,7 +0,0 @@
-import { createParent, createChild } from "../../src";
-
-export const parentComponent = createParent({
-  url: "http://localhost:3000/child.html",
-});
-
-export const childComponent = createChild({});
