c1ad72133e0efa770df7c70813e9335d1db17e73
Blade Barringer <blade.barringer@paypal.com>
2022-02-15 10:47:27 -0600

    feat: create base class for handling setting hooks and methods

---
+++ frame-component/src/__test__/frame-base-component.test.ts
@@ -0,0 +1,340 @@
+import { FrameBaseComponent } from "../frame-base-component";
+import { on, emitAsPromise, initialize, FramebusConfig } from "framebus";
+
+jest.mock("framebus");
+
+describe("FrameBaseComponent", () => {
+  let fakeConfig: FramebusConfig;
+
+  class SubComponent extends FrameBaseComponent {}
+
+  beforeEach(() => {
+    fakeConfig = {} as FramebusConfig;
+    jest.mocked(initialize).mockReturnValue(fakeConfig);
+    jest.mocked(emitAsPromise).mockResolvedValue({});
+  });
+
+  it("should create a bus config with provided channel", () => {
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: [],
+      hooks: {},
+    });
+
+    expect(initialize).toBeCalledTimes(1);
+    expect(initialize).toBeCalledWith({
+      channel: "custom-channel",
+    });
+  });
+
+  it("creates methods for each method name that is passed", () => {
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: ["foo", "bar"],
+      hooks: {},
+    });
+
+    expect(component.methods.foo).toEqual(expect.any(Function));
+    expect(component.methods.bar).toEqual(expect.any(Function));
+    expect(component.methods.notImplemented).toBeFalsy();
+
+    component.methods.foo("foo", "bar", "baz");
+
+    expect(emitAsPromise).toBeCalledTimes(1);
+    expect(emitAsPromise).toBeCalledWith(fakeConfig, "trigger-method-foo", {
+      args: ["foo", "bar", "baz"],
+    });
+
+    jest.mocked(emitAsPromise).mockClear();
+
+    component.methods.bar("foo", "bar", "baz");
+
+    expect(emitAsPromise).toBeCalledTimes(1);
+    expect(emitAsPromise).toBeCalledWith(fakeConfig, "trigger-method-bar", {
+      args: ["foo", "bar", "baz"],
+    });
+  });
+
+  it("creates methods that resolve when the emit call resolves", async () => {
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: ["foo"],
+      hooks: {},
+    });
+
+    jest.mocked(emitAsPromise).mockResolvedValue({ result: "foo" });
+
+    const result = await component.methods.foo("foo");
+
+    expect(result).toEqual("foo");
+  });
+
+  it("creates methods that reject with errors when the emit call resolves with an error string message", async () => {
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: ["foo"],
+      hooks: {},
+    });
+
+    jest
+      .mocked(emitAsPromise)
+      .mockResolvedValue({ error: "some error message" });
+
+    await expect(component.methods.foo("foo")).rejects.toEqual(
+      new Error("some error message")
+    );
+  });
+
+  it("creates methods that reject with objects when the emit call resolves with an error value other than string", async () => {
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: ["foo"],
+      hooks: {},
+    });
+
+    jest
+      .mocked(emitAsPromise)
+      .mockResolvedValue({ error: { message: "some error message" } });
+
+    await expect(component.methods.foo("foo")).rejects.toEqual({
+      message: "some error message",
+    });
+  });
+
+  it("creates listener for each hook defined", () => {
+    const fooSpy = jest.fn();
+    const barSpy = jest.fn();
+
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: [],
+      hooks: {
+        foo: fooSpy,
+        bar: barSpy,
+      },
+    });
+
+    expect(on).toBeCalledTimes(2);
+    expect(on).toBeCalledWith(
+      fakeConfig,
+      "trigger-method-foo",
+      expect.any(Function)
+    );
+    expect(on).toBeCalledWith(
+      fakeConfig,
+      "trigger-method-bar",
+      expect.any(Function)
+    );
+
+    const fooCallback = jest.mocked(on).mock.calls.find((args) => {
+      return args[1] === "trigger-method-foo";
+    })![2];
+
+    fooCallback(
+      {
+        args: ["foo", "bar", "baz"],
+      },
+      jest.fn()
+    );
+
+    expect(fooSpy).toBeCalledTimes(1);
+    expect(fooSpy).toBeCalledWith("foo", "bar", "baz");
+
+    const barCallback = jest.mocked(on).mock.calls.find((args) => {
+      return args[1] === "trigger-method-bar";
+    })![2];
+
+    barCallback(
+      {
+        args: ["foo", "bar", "baz"],
+      },
+      jest.fn()
+    );
+
+    expect(barSpy).toBeCalledTimes(1);
+    expect(barSpy).toBeCalledWith("foo", "bar", "baz");
+  });
+
+  it("replies with a result object with whatever the hook resolves with", async () => {
+    const fooSpy = jest.fn().mockResolvedValue("foo");
+    const barSpy = jest.fn().mockReturnValue("bar");
+
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: [],
+      hooks: {
+        foo: fooSpy,
+        bar: barSpy,
+      },
+    });
+
+    const fooCallback = jest.mocked(on).mock.calls.find((args) => {
+      return args[1] === "trigger-method-foo";
+    })![2];
+
+    const fooReply = jest.fn();
+
+    await fooCallback(
+      {
+        args: ["foo", "bar", "baz"],
+      },
+      fooReply
+    );
+
+    expect(fooReply).toBeCalledTimes(1);
+    expect(fooReply).toBeCalledWith({
+      result: "foo",
+    });
+
+    const barCallback = jest.mocked(on).mock.calls.find((args) => {
+      return args[1] === "trigger-method-bar";
+    })![2];
+    const barReply = jest.fn();
+
+    await barCallback(
+      {
+        args: ["foo", "bar", "baz"],
+      },
+      barReply
+    );
+
+    expect(barReply).toBeCalledTimes(1);
+    expect(barReply).toBeCalledWith({
+      result: "bar",
+    });
+  });
+
+  it("replies with an error message when hook throws an error or rejects with an error", async () => {
+    const fooSpy = jest.fn().mockRejectedValue(new Error("some error"));
+    const barSpy = jest.fn().mockImplementation(() => {
+      throw new Error("some other error");
+    });
+
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: [],
+      hooks: {
+        foo: fooSpy,
+        bar: barSpy,
+      },
+    });
+
+    const fooCallback = jest.mocked(on).mock.calls.find((args) => {
+      return args[1] === "trigger-method-foo";
+    })![2];
+
+    const fooReply = jest.fn();
+
+    await fooCallback(
+      {
+        args: ["foo", "bar", "baz"],
+      },
+      fooReply
+    );
+
+    expect(fooReply).toBeCalledTimes(1);
+    expect(fooReply).toBeCalledWith({
+      error: "some error",
+    });
+
+    const barCallback = jest.mocked(on).mock.calls.find((args) => {
+      return args[1] === "trigger-method-bar";
+    })![2];
+    const barReply = jest.fn();
+
+    await barCallback(
+      {
+        args: ["foo", "bar", "baz"],
+      },
+      barReply
+    );
+
+    expect(barReply).toBeCalledTimes(1);
+    expect(barReply).toBeCalledWith({
+      error: "some other error",
+    });
+  });
+
+  it("replies with an error message when hook throws something that is not an error", async () => {
+    const fooSpy = jest.fn().mockRejectedValue({
+      message: "not some error",
+    });
+    const barSpy = jest.fn().mockImplementation(() => {
+      throw { message: "not some other error" };
+    });
+
+    const component = new SubComponent({
+      channel: "custom-channel",
+      methods: [],
+      hooks: {
+        foo: fooSpy,
+        bar: barSpy,
+      },
+    });
+
+    const fooCallback = jest.mocked(on).mock.calls.find((args) => {
+      return args[1] === "trigger-method-foo";
+    })![2];
+
+    const fooReply = jest.fn();
+
+    await fooCallback(
+      {
+        args: ["foo", "bar", "baz"],
+      },
+      fooReply
+    );
+
+    expect(fooReply).toBeCalledTimes(1);
+    expect(fooReply).toBeCalledWith({
+      error: { message: "not some error" },
+    });
+
+    const barCallback = jest.mocked(on).mock.calls.find((args) => {
+      return args[1] === "trigger-method-bar";
+    })![2];
+    const barReply = jest.fn();
+
+    await barCallback(
+      {
+        args: ["foo", "bar", "baz"],
+      },
+      barReply
+    );
+
+    expect(barReply).toBeCalledTimes(1);
+    expect(barReply).toBeCalledWith({
+      error: { message: "not some other error" },
+    });
+  });
+
+  it("throws an error when hooks and methods have the same identifier", () => {
+    expect(() => {
+      new SubComponent({
+        channel: "custom-channel",
+        methods: ["foo"],
+        hooks: {
+          foo: jest.fn(),
+        },
+      });
+    }).toThrow(
+      new Error(
+        "Implementation Error: hooks and methods must have unique names"
+      )
+    );
+  });
+
+  it("throws an error when multiple methods with the same name are passed", () => {
+    expect(() => {
+      new SubComponent({
+        channel: "custom-channel",
+        methods: ["foo", "foo"],
+        hooks: {},
+      });
+    }).toThrow(
+      new Error(
+        "Implementation Error: multiple instances of method 'foo' detected"
+      )
+    );
+  });
+});
---
+++ frame-component/src/frame-base-component.ts
@@ -0,0 +1,119 @@
+import { initialize, FramebusConfig, on, emitAsPromise } from "framebus";
+
+type Hook = (...args: unknown[]) => void;
+type Methods = string[];
+type Hooks = {
+  [key: string]: Hook;
+};
+type HookResponse = {
+  error?: unknown;
+  result?: unknown;
+};
+
+export type FrameComponentProps = {
+  channel: string;
+  methods: Methods;
+  hooks: Hooks;
+};
+
+export abstract class FrameBaseComponent {
+  protected busConfig: FramebusConfig;
+  protected channel: string;
+
+  methods: Hooks = {};
+
+  constructor(options: FrameComponentProps) {
+    this.channel = options.channel;
+    this.busConfig = initialize({
+      channel: this.channel,
+    });
+
+    const methodsHaveOverlappingNameWithHooks = options.methods.find((name) => {
+      return name in options.hooks;
+    });
+
+    if (methodsHaveOverlappingNameWithHooks) {
+      throw new Error(
+        "Implementation Error: hooks and methods must have unique names"
+      );
+    }
+
+    this.setMethods(options.methods);
+    this.setHooks(options.hooks);
+  }
+
+  private setMethods(methods: Methods) {
+    for (const methodName of methods) {
+      if (this.methods[methodName]) {
+        throw new Error(
+          "Implementation Error: multiple instances of method 'foo' detected"
+        );
+      }
+
+      this.methods[methodName] = this.createMethod(methodName);
+    }
+  }
+
+  private createMethod(methodName: string) {
+    return async (...args: unknown[]) => {
+      const { error, result } = await emitAsPromise<HookResponse>(
+        this.busConfig,
+        `trigger-method-${methodName}`,
+        {
+          args,
+        }
+      );
+
+      if (error) {
+        return Promise.reject(this.deserializeError(error));
+      }
+      return result;
+    };
+  }
+
+  private setHooks(hooksMap: Hooks) {
+    for (const [methodName, hook] of Object.entries(hooksMap)) {
+      on(
+        this.busConfig,
+        `trigger-method-${methodName}`,
+        this.createHookCallback(hook)
+      );
+    }
+  }
+
+  private createHookCallback(hook: Hook): Parameters<typeof on>[2] {
+    return async (data, reply) => {
+      const args = data.args as unknown[];
+
+      try {
+        // wrap the result of the hook function in a promise
+        // so that it always returns a promise, even if the
+        // definition for the hook does not
+        const result = await Promise.resolve(hook(...args));
+
+        reply({
+          result,
+        });
+      } catch (error) {
+        reply({ error: this.serializeError(error) });
+      }
+    };
+  }
+
+  private serializeError(error: unknown) {
+    // errors can't be serialized over post message,
+    // so we send down just the message from the error
+    if (error instanceof Error) {
+      return error.message;
+    }
+
+    return error;
+  }
+
+  private deserializeError(error: unknown) {
+    if (typeof error === "string") {
+      return new Error(error);
+    }
+    return error;
+  }
+}
