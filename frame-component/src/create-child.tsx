21a8b4e6a79b3ec1c45c4bbf844f8c7013a405fc
cgdibble <cgdibble@gmail.com>
2022-02-23 16:44:52 -0800

    return new child directly

--- frame-component/src/create-child.tsx
+++ frame-component/src/create-child.tsx
@@ -1,7 +1,5 @@
 import { ChildComponent, ChildOptions } from "./child-component";
 
 export function createChild(options: ChildOptions) {
-  const component = new ChildComponent(options);
-
-  return component;
+  return new ChildComponent(options);
 }
