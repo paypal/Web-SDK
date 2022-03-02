814155f949e16d202517e7714279a5f441fb8219
Plukarski <joseph.plukarski@getbraintree.com>
2022-03-02 15:07:05 -0600

    chore: text input reports ready on mount

--- ui-components/hosted-fields/internal/components/__test__/text-input.test.tsx
+++ ui-components/hosted-fields/internal/components/__test__/text-input.test.tsx
@@ -1,13 +1,23 @@
 import { TextInput } from "../text-input";
-
+import { fakeChild } from "../../../__mocks__/frame-component";
 import { render, fireEvent, screen, waitFor } from "@testing-library/preact";
 
+jest.mock("frame-component");
+
 describe("TextInput", () => {
-  test("should render a text input", () => {
+  it("should render a text input", () => {
     render(<TextInput />);
 
     const input = screen.getByTestId("text-input");
 
     expect(input).toBeInstanceOf(HTMLInputElement);
+  });
+
+  it("should mark component as ready when it first renders", () => {
+    expect(fakeChild.reportReady).not.toBeCalled();
+
+    render(<TextInput />);
+
+    expect(fakeChild.reportReady).toBeCalledTimes(1);
   });
 });
--- ui-components/hosted-fields/internal/components/text-input.tsx
+++ ui-components/hosted-fields/internal/components/text-input.tsx
@@ -1,4 +1,13 @@
+import { createChild } from "frame-component";
+import { useEffect } from "preact/hooks";
+
+const child = createChild({});
+
 export function TextInput() {
+  useEffect(() => {
+    child.reportReady();
+  }, []);
+
   return (
     <>
       <input data-testid="text-input" />
